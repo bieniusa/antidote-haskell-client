fba34a37cda04c17f8fbf720b949882ee0599478
Annette Bieniusa <bieniusa@cs.uni-kl.de>
2017-10-07 14:37:35 +0200

    Work in progress: API

--- antidotedb/src/Database/Antidote.hs
+++ antidotedb/src/Database/Antidote.hs
@@ -1,4 +1,5 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, KindSignatures, DataKinds, TypeFamilies #-}
+{-# LANGUAGE BangPatterns, GeneralizedNewtypeDeriving #-}
 module Database.Antidote where
 
 import Antidote.ApbCounterUpdate
@@ -8,7 +9,7 @@ import Antidote.ApbTxnProperties
 import Antidote.ApbCommitResp
 import Antidote.ApbCommitTransaction
 import Antidote.ApbBoundObject
-import Antidote.CRDT_type
+import qualified Antidote.CRDT_type as A
 import Antidote.ApbReadObjects
 import Antidote.ApbUpdateOperation
 import Antidote.ApbUpdateOp
@@ -24,6 +25,72 @@ import qualified Data.ByteString.Lazy as L
 import Data.Binary as B
 import Data.Binary.Put as B
 import Data.Binary.Get as B
+import Control.Monad.Reader
+
+
+newtype Adb a = Adb { unAdb :: ReaderT AdbCtx IO a }
+  deriving (Functor, Applicative, Monad)
+
+data AdbCtx = AdbCtx !AdbConn !TxnDescriptor
+type TxnDescriptor = L.ByteString
+data AdbConn = AdbConn !Socket
+
+data AdbConfig = AdbConfig { adbHostName :: !String, adbPort :: !Int }
+  deriving (Eq, Show)
+
+withAdb :: AdbConfig -> (AdbConn -> IO a) -> IO a
+withAdb cfg f
+  = error "todo"
+
+runAdbTxn :: AdbConn -> Adb a -> IO a
+runAdbTxn conn m
+  = error "todo"
+
+data CrdtType = CrdtCounter
+
+class SingCrdtType (t :: CrdtType) where
+  singCrdtType :: sing t -> CrdtType
+
+instance SingCrdtType 'CrdtCounter where
+  singCrdtType _ = CrdtCounter
+
+data CRDT (t :: CrdtType) = CRDT !L.ByteString !L.ByteString
+
+counterCrdt :: L.ByteString -> L.ByteString -> CRDT 'CrdtCounter
+counterCrdt = CRDT
+
+class SingCrdtType t => IsCrdt (t :: CrdtType) where
+  type CrdtRep t
+  data CrdtUpdate t
+  decodeState  :: sing t -> ApbReadObjectResp -> Either String (CrdtRep t)
+  encodeUpdate :: CrdtUpdate t -> ApbUpdateOperation
+
+instance IsCrdt 'CrdtCounter where
+  type CrdtRep    'CrdtCounter = Int
+  data CrdtUpdate 'CrdtCounter = CounterInc !Int
+  decodeState  = error "todo"
+  encodeUpdate = error "todo"
+
+incCounter :: Int -> CrdtUpdate 'CrdtCounter
+incCounter = CounterInc
+
+readCrdt :: IsCrdt t => CRDT t -> Adb (CrdtRep t)
+readCrdt = error "todo"
+
+updateCrdt :: IsCrdt t => CRDT t -> CrdtUpdate t -> Adb ()
+updateCrdt = error "todo"
+
+
+test2 :: IO ()
+test2
+  = withAdb cfg $ \conn ->
+      do c <- runAdbTxn conn $ do
+                updateCrdt obj (incCounter 1)
+                readCrdt obj
+         putStrLn (show c)
+  where
+    cfg = AdbConfig "127.0.0.1" 8087
+    obj = counterCrdt "bucket" "x"
 
 
 
@@ -55,7 +122,7 @@ test = do
             let (Just d) = Antidote.ApbStartTransactionResp.transactionDescriptor answer
 
             -- inc counter
-            let bo = ApbBoundObject "key" Counter "bucket"
+            let bo = ApbBoundObject "key" A.Counter "bucket"
             let inc = ApbCounterUpdate (Just 1)
             let oper = defaultVal{counterop = Just inc}
             let op = ApbUpdateOp bo oper
